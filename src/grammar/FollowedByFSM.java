61a3ec33f0dfa5edce3e66fd89e206110df1590b
Dom Parfitt <dom.parfitt@cgi.com>
2017-10-02 13:14:11 +0100

    Simplified constructors and added initialise method to handle shared logic

--- src/grammar/FollowedByFSM.java
+++ src/grammar/FollowedByFSM.java
@@ -1,11 +1,37 @@
 package grammar;
 
+import com.sun.deploy.util.StringUtils;
+
 import java.util.List;
 
 public class FollowedByFSM extends FiniteStateMachine{
 
+    public FollowedByFSM(String characters) {
+        super(characters);
+        List<Character> charList = FiniteStateMachine.stringToCharList(characters);
+        this.initialise(charList);
+//        State currentState = this.initialState;
+//        for(Character character: characters.toCharArray()) {
+//            State nextState = new State(false);
+//            currentState.addTransition(character, nextState);
+//            currentState = nextState;
+//        }
+//        currentState.setIsAcceptingState(true);
+    }
+
     public FollowedByFSM(List<Character> characters) {
         super(characters);
+        this.initialise(characters);
+//        State currentState = this.initialState;
+//        for(Character character: characters) {
+//            State nextState = new State(false);
+//            currentState.addTransition(character, nextState);
+//            currentState = nextState;
+//        }
+//        currentState.setIsAcceptingState(true);
+    }
+
+    private void initialise(List<Character> characters) {
         State currentState = this.initialState;
         for(Character character: characters) {
             State nextState = new State(false);
--- src/grammar/UnionFSM.java
+++ src/grammar/UnionFSM.java
@@ -1,16 +1,30 @@
 package grammar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UnionFSM extends FiniteStateMachine {
 
-//    public UnionFSM(String characters) {
-//        super(characters);
-//
-//    }
+    public UnionFSM(String characters) {
+        super(characters);
+        List<Character> charList = FiniteStateMachine.stringToCharList(characters);
+        this.initialise(charList);
+//        State acceptingState = new State(true);
+//        for(Character character : characters.toCharArray()) {
+//            this.initialState.addTransition(character, acceptingState);
+//        }
+    }
 
     public UnionFSM(List<Character> characters) {
         super(characters);
+        this.initialise(characters);
+//        State acceptingState = new State(true);
+//        for(Character character : characters) {
+//            this.initialState.addTransition(character, acceptingState);
+//        }
+    }
+
+    private void initialise(List<Character> characters) {
         State acceptingState = new State(true);
         for(Character character : characters) {
             this.initialState.addTransition(character, acceptingState);
