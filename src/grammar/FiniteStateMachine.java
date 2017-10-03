fd31809c0d9e3929cfaded3cbc970330f89b540b
Dom Parfitt <dom.parfitt@cgi.com>
2017-10-03 14:34:46 +0100

    Marked some unused methods as deprecated

--- src/grammar/FiniteStateMachine.java
+++ src/grammar/FiniteStateMachine.java
@@ -1,10 +1,7 @@
 package grammar;
 
 import java.io.FileInputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Abstract base class for a finite state machine
@@ -16,6 +13,7 @@ public abstract class FiniteStateMachine {
 
     /**
      * Initialises a FiniteStateMachine using a list of characters
+     *
      * @param characters the characters which are used for transitioning between states
      */
     public FiniteStateMachine(List<Character> characters) {
@@ -26,14 +24,16 @@ public abstract class FiniteStateMachine {
     /**
      * Convenience constructor to initialise an FSM using a string in place of a list
      * of characters
+     *
      * @param characters the characters which are used for transitioning between states
      */
     public FiniteStateMachine(String characters) {
         this(new ArrayList<>());
         char[] chars = characters.toCharArray();
-        for(char character : chars) {
+        for (char character : chars) {
             this.characters.add(character);
         }
+//        this.characters.add(Arrays.asList(chars));
     }
 
 
@@ -48,20 +48,21 @@ public abstract class FiniteStateMachine {
     /**
      * Parses the input string and determines whether it is a valid string
      * for the transitions within the FSM
+     *
      * @param input the string to test
      * @return true if the string is valid according to the FSM, false otherwise
      */
     public final boolean parse(String input) {
         State currentState = this.initialState;
-        for(int i = 0; i < input.length(); i++) {
-          char character = input.charAt(i);
+        for (int i = 0; i < input.length(); i++) {
+            char character = input.charAt(i);
 //          String character = input.substring(i, i+1);
-          if(currentState.hasTransition(character)) {
-              currentState = currentState.getResultingState(character);
-          } else {
-              //FSM cannot transition with current character, therefore illegal character
-              return false;
-          }
+            if (currentState.hasTransition(character)) {
+                currentState = currentState.getResultingState(character);
+            } else {
+                //FSM cannot transition with current character, therefore illegal character
+                return false;
+            }
         }
 
         return currentState.isAcceptingState();
@@ -70,7 +71,7 @@ public abstract class FiniteStateMachine {
     public final void combine(FiniteStateMachine next) {
         State finalState = this.getFinalState();
         finalState.setIsAcceptingState(false);
-        for(Map.Entry<Character, State> transition : next.initialState.getTransitions().entrySet()) {
+        for (Map.Entry<Character, State> transition : next.initialState.getTransitions().entrySet()) {
 //            State nextState = transition.getValue();
 //            nextState.setIsAcceptingState(true);
             finalState.addTransition(transition.getKey(), transition.getValue());
@@ -82,22 +83,29 @@ public abstract class FiniteStateMachine {
 
     /**
      * Gets the final state of the FSM, a.k.a the accepting state
+     *
      * @return the final state
      */
+    @Deprecated
     public State getFinalState() {
         State currentState = this.initialState;
-        while(!currentState.isAcceptingState()) {
+        while (!currentState.isAcceptingState()) {
             currentState = currentState.getNextState();
         }
 
         return currentState;
     }
 
+    public Set<State> getFinalStates() {
+        return this.initialState.getFinalStates();
+    }
+
+    @Deprecated
     public State getPenultimateState() {
         State currentState = this.initialState;
-        while(currentState.hasNextState()) {
+        while (currentState.hasNextState()) {
             State nextState = currentState.getNextState();
-            if(nextState.isAcceptingState()) {
+            if (nextState.isAcceptingState()) {
                 return currentState;
             }
         }
@@ -105,10 +113,27 @@ public abstract class FiniteStateMachine {
         return currentState;
     }
 
+    @Deprecated
+    public Set<State> getPenultimateStates() {
+        Set<State> penultimates = new HashSet<>();
+        State currentState = this.initialState;
+        while (currentState.hasNextState()) {
+            Set<State> nextStates = currentState.getNextStates();
+            for (State nextState : nextStates) {
+
+                if (nextState.isAcceptingState()) {
+                    penultimates.add(currentState);
+                }
+            }
+        }
+
+        return penultimates;
+    }
+
     protected String toString(String separator) {
         String output = ""; //"(";
         String prefix = "";
-        for(Character character : this.characters) {
+        for (Character character : this.characters) {
             output += prefix + character;
             prefix = separator;
         }
@@ -118,7 +143,7 @@ public abstract class FiniteStateMachine {
 
     protected static List<Character> stringToCharList(String characters) {
         List<Character> characterList = new ArrayList<>();
-        for(Character character : characters.toCharArray()) {
+        for (Character character : characters.toCharArray()) {
             characterList.add(character);
         }
 
