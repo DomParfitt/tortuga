728ef21c3d8bc6a7b620c46589be24e259c9bdb3
Dom Parfitt <dom.parfitt@cgi.com>
2017-10-05 15:50:08 +0100

    Added a test to check valid identifiers

--- test/lexer/TestLexer.java
+++ test/lexer/TestLexer.java
@@ -2,11 +2,13 @@ package lexer;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 
 public class TestLexer {
@@ -67,18 +69,76 @@ public class TestLexer {
     }
 
     @Test
+    public void testValidIdentifiers() {
+        String input;
+        List<Token> expected = new ArrayList<>();
+        List<Token> actual;
+
+        //Passes on alphabetic identifier
+        input = "var";
+        lexer = new Lexer(input);
+        expected.add(new Token(TokenType.IDENTIFIER, "var", 0, 0));
+        actual = lexer.tokenize();
+        this.assertListEquality(expected, actual);
+        expected.clear();
+
+        //Passes on alphanumeric identifier
+        input = "var123";
+        lexer = new Lexer(input);
+        expected.add(new Token(TokenType.IDENTIFIER, "var123", 0, 0));
+        actual = lexer.tokenize();
+        this.assertListEquality(expected, actual);
+        expected.clear();
+
+        //Passes on identifier beginning with underscore
+        input = "_var123";
+        lexer = new Lexer(input);
+        expected.add(new Token(TokenType.IDENTIFIER, "_var123", 0, 0));
+        actual = lexer.tokenize();
+        this.assertListEquality(expected, actual);
+        expected.clear();
+
+        //Passes on alphanumeric containing underscore
+        input = "var_123";
+        lexer = new Lexer(input);
+        expected.add(new Token(TokenType.IDENTIFIER, "var_123", 0, 0));
+        actual = lexer.tokenize();
+        this.assertListEquality(expected, actual);
+        expected.clear();
+
+        //Fails on identifier containing invalid character
+        try {
+            input = "var_1?23";
+            lexer = new Lexer(input);
+            actual = lexer.tokenize();
+            fail();
+        } catch (TokenizeException e) {
+            System.out.println(e.getMessage());
+        }
+
+        //"Fails" on identifier which is a keyword (checks the category return is keyword, not identifier)
+        input = "if";
+        lexer = new Lexer(input);
+        expected.add(new Token(TokenType.IF, "if", 0, 0));
+        actual = lexer.tokenize();
+        Token token = actual.get(0);
+        assertTrue(token.getTokenCategory() == TokenCategory.KEYWORD);
+    }
+
+    @Test
     public void testLexer() {
-        lexer = new Lexer("if var = ( 1 + 2 )");
+        lexer = new Lexer("if _var123 = ( 1 + 2 )");
         List<Token> tokens = lexer.tokenize();
-        for(Token token : tokens) {
+        for (Token token : tokens) {
             System.out.println(token);
         }
     }
 
-    private void assertListEquality(List<String> expected, List<String> actual) {
+    private <T> void assertListEquality(List<T> expected, List<T> actual) {
         assertEquals(expected.size(), actual.size());
-        for(String token : expected) {
+        for (T token : expected) {
             assertTrue(actual.contains(token));
         }
     }
+
 }
