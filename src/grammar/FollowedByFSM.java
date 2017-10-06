403e7470b53e6e149f774653fed237a546d4995c
Dom Parfitt <domparfitt89@hotmail.com>
2017-10-06 22:22:31 +0100

    Reimplementation of FollowedBy

--- src/grammar/FollowedByFSM.java
+++ src/grammar/FollowedByFSM.java
@@ -11,71 +11,42 @@ import java.util.Set;
  */
 public class FollowedByFSM extends FiniteStateMachine {
 
-    /**
-     * Initialise using a string of characters
-     * @param characters the characters to use for parsing
-     */
+
     public FollowedByFSM(String characters) {
-        super(characters);
-        List<Character> charList = FiniteStateMachine.stringToCharList(characters);
-        this.initialise(charList);
+        super();
+        for(Character character : characters.toCharArray()) {
+            State nextState = new State(this.stateCounter++, false);
+            this.states.add(nextState);
+            this.terminalStateIndex = this.stateCounter - 1;
+            Transition transition = new Transition(character, this.getCurrentState(), nextState);
+            this.transitions.add(transition);
+            this.setCurrentState(nextState);
+        }
+        this.getTerminalState().setAcceptingState(true);
+
     }
 
-    /**
-     * Initialise with a list of characters
-     * @param characters the characters to use for parsing
-     */
-    public FollowedByFSM(List<Character> characters) {
-        super(characters);
-        this.initialise(characters);
-    }
-
-    /**
-     * Initialise this FSM by compounding two other FSMs. Results in an FSM
-     * which validates a string on the first followed by the second
-     * @param first the first FSM
-     * @param second the second FSM
-     */
     public FollowedByFSM(FiniteStateMachine first, FiniteStateMachine second) {
-        FiniteStateMachine firstCopy, secondCopy;
-        firstCopy = first.copy();
-        secondCopy = second.copy();
-        this.initialState = firstCopy.initialState;
-        Set<State> finalStates = firstCopy.getFinalStates();
-        for (State finalState : finalStates) {
-            for (Map.Entry<Character, State> transition : secondCopy.initialState.getTransitions().entrySet()) {
 
-                finalState.addTransition(transition.getKey(), transition.getValue());
-
-            }
-
-            //If the initial state of the 2nd FSM is accepting then the final state can be as well
-            finalState.setIsAcceptingState(secondCopy.initialState.isAcceptingState());
-        }
-    }
-
-    private void initialise(List<Character> characters) {
-        State currentState = this.initialState;
-        for (Character character : characters) {
-            State nextState = new State(false, this.stateCounter++);
-            currentState.addTransition(character, nextState);
-            currentState = nextState;
-        }
-        currentState.setIsAcceptingState(true);
     }
 
     @Override
-    //TODO: This handles a basic FollowedBy but not any kind of compound
+    public void addTransition(Character character) {
+        //TODO: Implement
+    }
+
+    @Override
+    public void addState(State state) {
+        //TODO: Implement
+    }
+
+    @Override
     public FiniteStateMachine copy() {
-        FiniteStateMachine copy = new FollowedByFSM(this.characters);
-        copy.initialState = this.initialState.copy();
+        FiniteStateMachine copy = new FollowedByFSM("");
+        copy.terminalStateIndex = this.terminalStateIndex;
+        copy.states = this.copyStates();
+        copy.transitions = this.copyTransitions(copy.states);
+
         return copy;
-//        return new FollowedByFSM(this.characters);
     }
-
-    @Override
-    public String toString() {
-        return super.toString("");
-    }
-
 }
