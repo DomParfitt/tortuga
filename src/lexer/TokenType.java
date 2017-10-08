215338ef61bd318302d6f7d5f305a49ad0dd3c09
Dom Parfitt <domparfitt89@hotmail.com>
2017-10-08 17:09:01 +0100

    Reordered some of the elements to better represent the precendence

--- src/lexer/TokenType.java
+++ src/lexer/TokenType.java
@@ -8,16 +8,13 @@ public enum TokenType {
     LETTER(TokenCategory.LITERAL, new UnionFSM("abcdefghifjklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")),
     DIGIT(TokenCategory.LITERAL, new UnionFSM("0123456789")),
 
+    //Identifiers
+    IDENTIFIER(TokenCategory.IDENTIFIER, FSMFactory.getIdentifierFSM()),
+
     //Separators
     PARENTHESES(TokenCategory.SEPARATOR, new UnionFSM("()[]{}")),
     COMMA(TokenCategory.SEPARATOR, new UnionFSM(",")),
     SEMICOLON(TokenCategory.SEPARATOR, new UnionFSM(";")),
-
-    //Keywords
-    IF(TokenCategory.KEYWORD, new FollowedByFSM("if")),
-    ELSE(TokenCategory.KEYWORD, new FollowedByFSM("else")),
-    WHILE(TokenCategory.KEYWORD, new FollowedByFSM("while")),
-    FOR(TokenCategory.KEYWORD, new FollowedByFSM("for")),
 
     //Operators
     PLUS(TokenCategory.OPERATOR, new FollowedByFSM("+")),
@@ -31,8 +28,11 @@ public enum TokenType {
     LESSTHAN(TokenCategory.OPERATOR, new FollowedByFSM("<")),
     LESSTHANEQUALS(TokenCategory.OPERATOR, new FollowedByFSM("<=")),
 
-    //Identifiers
-    IDENTIFIER(TokenCategory.IDENTIFIER, FSMFactory.getIdentifierFSM())
+    //Keywords
+    IF(TokenCategory.KEYWORD, new FollowedByFSM("if")),
+    ELSE(TokenCategory.KEYWORD, new FollowedByFSM("else")),
+    WHILE(TokenCategory.KEYWORD, new FollowedByFSM("while")),
+    FOR(TokenCategory.KEYWORD, new FollowedByFSM("for"))
     ;
 
     private String value; //TODO: Not sure if this is necessary
@@ -45,6 +45,10 @@ public enum TokenType {
     }
 
     public boolean parse(String input) {
+        return this.machine.parse(input);
+    }
+
+    public boolean parse(Character input) {
         return this.machine.parse(input);
     }
 
