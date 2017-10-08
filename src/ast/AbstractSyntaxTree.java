5a67a9b6d8685082c3e8c0c3f0ebebe075002114
Dom Parfitt <domparfitt89@hotmail.com>
2017-10-08 19:45:12 +0100

    Further implementation for AST

--- src/ast/AbstractSyntaxTree.java
+++ src/ast/AbstractSyntaxTree.java
@@ -8,10 +8,62 @@ import java.util.List;
 public class AbstractSyntaxTree {
 
     private Token token;
+    private AbstractSyntaxTree parent, left, right;
+    //Not sure if it will ever need more than two branches
     private List<AbstractSyntaxTree> branches;
+
+    public AbstractSyntaxTree() {
+
+    }
 
     public AbstractSyntaxTree(Token token) {
         this.token = token;
         this.branches = new ArrayList<>();
+    }
+
+    public Token getToken() {
+        return token;
+    }
+
+    public void setToken(Token token) {
+        this.token = token;
+    }
+
+    public AbstractSyntaxTree getParent() {
+        return this.parent;
+    }
+
+    public void setParent(AbstractSyntaxTree parent) {
+        this.parent = parent;
+    }
+
+    public AbstractSyntaxTree getLeftChild() {
+        return this.left;
+    }
+
+    public AbstractSyntaxTree getRightChild() {
+        return this.right;
+    }
+
+    public void setLeftChild(AbstractSyntaxTree left) {
+        left.parent = this;
+        if (this.left != null) {
+            this.left.parent = left;
+        }
+        left.left = this.left;
+        this.left = left;
+    }
+
+    public void setRightChild(AbstractSyntaxTree right) {
+        right.parent = this;
+        if (this.right != null) {
+            this.right.parent = right;
+        }
+        right.right = this.right;
+        this.right = right;
+    }
+
+    private void addChild(AbstractSyntaxTree branch) {
+
     }
 }
