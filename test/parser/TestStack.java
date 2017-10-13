6cdbe6cda28f373a6ecc66892ef89187d5e456ac
Dom Parfitt <dom.parfitt@cgi.com>
2017-10-13 10:44:09 +0100

    Created a unit test class for Stack

---
+++ test/parser/TestStack.java
@@ -0,0 +1,41 @@
+package parser;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class TestStack {
+
+    private Stack<String> stack;
+
+    @Before
+    public void setUp() {
+        this.stack = new Stack<>();
+    }
+
+    @Test
+    public void testPushAndPopSingle() {
+        this.stack.push("Test");
+        assertEquals("Test", this.stack.pop());
+    }
+
+    @Test
+    public void testPushMultiplePopSingle() {
+        this.stack.push("Test1");
+        this.stack.push("Test2");
+        this.stack.push("Test3");
+        assertEquals("Test3", this.stack.pop());
+    }
+
+    @Test
+    public void testPopEmptyStack() {
+        try {
+            this.stack.pop();
+            fail();
+        } catch (StackUnderflowError e) {
+
+        }
+    }
+}
