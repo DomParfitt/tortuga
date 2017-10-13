249fa3734fd4f2ac9a3f426daf837f053fe23421
Dom Parfitt <dom.parfitt@cgi.com>
2017-10-13 16:21:16 +0100

    Removed inheritence from FSM

--- src/automata/PushdownAutomaton.java
+++ src/automata/PushdownAutomaton.java
@@ -1,15 +1,13 @@
 package automata;
 
-import automata.FiniteStateMachine;
 import lexer.Token;
 import parser.Stack;
 
-public class PushdownAutomaton extends FiniteStateMachine {
+import java.util.Set;
 
+public class PushdownAutomaton {
+
+    protected Set<State> states;
     protected Stack<Token> stack;
 
-    @Override
-    public FiniteStateMachine copy() {
-        return null;
-    }
 }
