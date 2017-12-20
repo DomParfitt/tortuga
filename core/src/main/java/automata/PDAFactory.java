7e84eb0f9c9f8d8525420b84e750a5fe3001e99c
Dom Parfitt <domparfitt89@hotmail.com>
2017-12-20 14:54:23 +0000

    Created PDAFactory class

---
+++ core/src/main/java/automata/PDAFactory.java
@@ -0,0 +1,45 @@
+package automata;
+
+import lexer.Token;
+import lexer.TokenType;
+
+import java.util.*;
+
+public class PDAFactory {
+
+    private static PushdownAutomaton mathematicalStatement;
+
+    public static PushdownAutomaton getMathematicalStatement() {
+        if(PDAFactory.mathematicalStatement == null) {
+            List<State> states = PDAFactory.getListOfStates(4, Arrays.asList(3));
+
+            Set<PDATransition> transitions = new HashSet<>();
+            transitions.add(new PDATransition(TokenType.DIGIT, new StackAction(StackAction.StackActionType.NONE), states.get(0), states.get(1)));
+            transitions.add(new PDATransition(TokenType.PLUS, new StackAction(StackAction.StackActionType.NONE), states.get(1), states.get(2)));
+            transitions.add(new PDATransition(TokenType.DIGIT, new StackAction(StackAction.StackActionType.NONE), states.get(2), states.get(3)));
+
+//            PDAFactory.mathematicalStatement = new PushdownAutomaton(states, transitions);
+            return new PushdownAutomaton(states, transitions);
+
+        }
+            return PDAFactory.mathematicalStatement;
+
+    }
+
+    private static List<State> getListOfStates(int numberOfStates, List<Integer> acceptingStates) {
+        List<State> states = new ArrayList<>();
+        for(int i = 0; i < numberOfStates; i++) {
+
+            State state;
+
+            if(acceptingStates.contains(i)) {
+                state = new State(i, true);
+            } else {
+                state = new State(i, false);
+            }
+            states.add(state);
+        }
+
+        return states;
+    }
+}
