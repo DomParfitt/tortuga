edfaa28b19fd398e93b5c598f7fcb473aa8629f4
Dom Parfitt <domparfitt89@hotmail.com>
2017-12-20 14:56:13 +0000

    Further implementations to Grammar

--- core/src/main/java/parser/Grammar.java
+++ core/src/main/java/parser/Grammar.java
@@ -1,7 +1,18 @@
 package parser;
 
+import automata.PushdownAutomaton;
+
 public enum Grammar {
 
-    STATEMENT,
+    STATEMENT(new PushdownAutomaton());
 
+    PushdownAutomaton machine;
+
+    Grammar(PushdownAutomaton machine) {
+        this.machine = machine;
+    }
+
+    public PushdownAutomaton getMachine() {
+        return machine;
+    }
 }
