3bb6b065ffdb4c0d9a48533dfeb086db0651585a
Dom Parfitt <domparfitt89@hotmail.com>
2017-12-22 21:47:14 +0000

    Created an interface to represent different grammars

---
+++ core/src/main/java/lexer/Grammar.java
@@ -0,0 +1,14 @@
+package lexer;
+
+import automata.FiniteStateMachine;
+
+import java.util.List;
+
+public interface Grammar<T> {
+
+//    public boolean parse(T input);
+
+    public boolean parse(List<T> input);
+
+    public FiniteStateMachine<T> getMachine();
+}
