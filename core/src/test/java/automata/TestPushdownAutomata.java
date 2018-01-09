b9a2a45674b2f9ee55a0a875d1300c0603ce397e
Dom Parfitt <dom.parfitt@cgi.com>
2018-01-09 15:00:36 +0000

    Added new tests

--- core/src/test/java/automata/TestPushdownAutomata.java
+++ core/src/test/java/automata/TestPushdownAutomata.java
@@ -8,7 +8,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.Assert.assertTrue;
 
@@ -35,5 +37,52 @@ public class TestPushdownAutomata {
 
         assertTrue(pda.parse(lexerGrammars));
         
+    }
+
+    @Test
+    public void testConcatenate() {
+        List<State> states = new ArrayList<>();
+        for(int i = 0; i <= 1; i++) {
+            State state = new State(i, false);
+            if(i == 0) {
+                state.setCurrentState(true);
+            }
+
+            if(i == 1) {
+                state.setAcceptingState(true);
+            }
+            states.add(state);
+        }
+
+        Set<Transition<LexerGrammar>> transitions = new HashSet<>();
+        transitions.add(new PDATransition(LexerGrammar.MINUS, new StackAction(StackAction.StackActionType.NONE), states.get(0), states.get(1)));
+        pda = new PushdownAutomaton(states, transitions);
+
+
+        List<State> states2 = new ArrayList<>();
+        for(int i = 0; i <= 1; i++) {
+            State state = new State(i, false);
+            if(i == 0) {
+                state.setCurrentState(true);
+            }
+            if(i == 1) {
+                state.setAcceptingState(true);
+            }
+            states2.add(state);
+        }
+
+        Set<Transition<LexerGrammar>> transitions2 = new HashSet<>();
+        transitions2.add(new PDATransition(LexerGrammar.INT_LITERAL, new StackAction(StackAction.StackActionType.NONE), states2.get(0), states2.get(1)));
+
+        PushdownAutomaton pda2 = new PushdownAutomaton(states2, transitions2);
+
+        PushdownAutomaton compound = PDAFactory.concatenate(pda, pda2);
+
+        List<LexerGrammar> tokens = new ArrayList<>();
+        tokens.add(LexerGrammar.MINUS);
+        tokens.add(LexerGrammar.INT_LITERAL);
+
+        assertTrue(compound.parse(tokens));
+
     }
 }
