fb3c23ac61b0f4f107602b6116364dc987ae2c68
Dom Parfitt <dom.parfitt@cgi.com>
2018-01-10 13:23:51 +0000

    Removed references to Transitions

--- core/src/main/java/automata/StackAction.java
+++ core/src/main/java/automata/StackAction.java
@@ -10,13 +10,15 @@ public class StackAction {
 
     private StackActionType stackActionType;
     private LexerGrammar token;
+//    private Transition<LexerGrammar> transition;
 
     public StackAction(StackActionType stackActionType) {
-        this(stackActionType, null);
+        this.stackActionType = stackActionType;
+//        this.transition = transition;
     }
 
     public StackAction(StackActionType stackActionType, LexerGrammar token) {
-        this.stackActionType = stackActionType;
+        this(stackActionType);
         this.token = token;
     }
 
@@ -27,4 +29,8 @@ public class StackAction {
     public LexerGrammar getTokenType() {
         return token;
     }
+
+//    public boolean appliesTo(Transition<LexerGrammar> transition) {
+//        return this.transition.equals(transition);
+//    }
 }
