daf6763aeefa08c0b24cbd7097c14543302fdd83
Dom Parfitt <dom.parfitt@cgi.com>
2018-01-10 15:16:23 +0000

    Created convenience classes for constructing PDAs

---
+++ core/src/main/java/automata/Pop.java
@@ -0,0 +1,12 @@
+package automata;
+
+import grammar.LexerGrammar;
+
+public class Pop extends PushdownAutomaton {
+
+    public Pop(LexerGrammar token, LexerGrammar expected) {
+        super();
+        this.addState(true);
+        this.addTransition(this.getInitialState(), this.getTerminalState(), token, StackAction.StackActionType.POP, expected);
+    }
+}
