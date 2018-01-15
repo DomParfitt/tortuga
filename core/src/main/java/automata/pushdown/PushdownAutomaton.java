f67e6591067007c989465cbce0e1672079a3fc01
Dom Parfitt <dom.parfitt@cgi.com>
2018-01-15 13:17:39 +0000

    Made PDA an abstract generic base class

--- core/src/main/java/automata/pushdown/PushdownAutomaton.java
+++ core/src/main/java/automata/pushdown/PushdownAutomaton.java
@@ -1,128 +1,41 @@
 package automata.pushdown;
 
-import automata.State;
-import automata.actions.Transition;
 import automata.finitestate.FiniteStateMachine;
 import automata.memory.Stack;
-import grammar.LexerGrammar;
-import grammar.ParserGrammar;
+import grammar.Grammar;
 
 import java.util.List;
-import java.util.Set;
 
-public class PushdownAutomaton extends FiniteStateMachine<LexerGrammar> {
+/**
+ * Extension of FiniteStateMachine with memory added in the form of a stack
+ */
+public abstract class PushdownAutomaton<T> extends FiniteStateMachine<T> {
 
-    protected Stack<LexerGrammar> stack;
-//    protected Map<Transition<LexerGrammar>, StackAction> actions;
+    protected Stack<T> stack;
 
     public PushdownAutomaton() {
-        //TODO: Implement
         super();
         this.stack = new Stack<>();
-//        this.actions = new HashMap<>();
     }
 
-    @Deprecated
-    public PushdownAutomaton(Set<State> states, Set<Transition<LexerGrammar>> transitions) {
-        this();
-//        this.states = states;
-//        this.transitions = transitions;
+    public PushdownAutomaton<T> concatenate(PushdownAutomaton other) {
+        return (PushdownAutomaton<T>) super.concatenate(other);
     }
 
-    @Override
-    public boolean parse(List<LexerGrammar> tokens) {
-//        for(LexerGrammar token : tokens) {
-//            if(this.hasTransition(token)) {
-//                Transition<LexerGrammar> transition = this.getTransition(token);
-//                StackAction action = (StackAction) transition.getAction();
-//
-//                //Do any required actions on the stack
-//                switch (action.getStackActionType()) {
-//                    case PUSH: {
-//                        this.stack.push(token);
-//                        break;
-//                    }
-//                    case POP: {
-//                        boolean isExpectedToken = this.popAndCompare(action.getTokenType());
-//                        if(!isExpectedToken) {
-//                            return false;
-//                        }
-//                        break;
-//                    }
-//                    case BOTH: {
-//                        boolean isExpectedToken = this.popAndCompare(action.getTokenType());
-//                        if(!isExpectedToken) {
-//                            return false;
-//                        }
-//                        this.stack.push(token);
-//                        break;
-//                    }
-//                    case NONE: {
-//                        break;
-//                    }
-//                }
-//
-//                //Set the resulting state as the current state
-//                this.setCurrentState(transition.getResultingState());
-//            }
-//        }
-
-        return this.getCurrentState().isAcceptingState() && this.stack.isEmpty();
+    public PushdownAutomaton<T> concatenate(Grammar grammar) {
+        return (PushdownAutomaton<T>)super.concatenate(grammar.getMachine());
     }
 
-    public PushdownAutomaton concatenate(PushdownAutomaton other) {
-        return (PushdownAutomaton) super.concatenate(other);
-//        for(Map.Entry<Transition<LexerGrammar>, StackAction> action : other.actions.entrySet()) {
-//            for(LexerGrammar token : action.getKey().getInputSymbols()) {
-//                Transition<LexerGrammar> transition = pda.getTransition(token);
-//                pda.actions.put(transition, action.getValue());
-//            }
-////            pda.actions.put(action.getKey(), action.getValue());
-//        }
-//        return pda;
+    public PushdownAutomaton<T> union(PushdownAutomaton other) {
+        return (PushdownAutomaton<T>) super.union(other);
     }
 
-    public PushdownAutomaton concatenate(ParserGrammar grammar) {
-        return this.concatenate(grammar.getMachine());
+    public PushdownAutomaton<T> union(Grammar grammar) {
+        return (PushdownAutomaton<T>) super.union(grammar.getMachine());
     }
 
-    public PushdownAutomaton union(PushdownAutomaton other) {
-        return (PushdownAutomaton) super.union(other);
-    }
-
-    public PushdownAutomaton union(ParserGrammar grammar) {
-        return this.union(grammar.getMachine());
-    }
-
-    public PushdownAutomaton loop() {
-        return (PushdownAutomaton) super.loop();
-    }
-
-//    public void addTransition(State fromState, State toState, LexerGrammar token, StackAction.StackActionType stackActionType, LexerGrammar actionToken) {
-//        StackAction stackAction = new StackAction(stackActionType, actionToken);
-//        Transition<LexerGrammar> transition = new Transition<>(token, fromState, toState, stackAction);
-//        super.addTransition(transition);
-////        this.addAction(transition, stackActionType, actionToken);
-//    }
-
-//    public void addAction(Transition<LexerGrammar> transition, StackAction.StackActionType stackActionType, LexerGrammar actionToken) {
-//        StackAction stackAction = new StackAction(stackActionType, actionToken);
-//        this.actions.put(transition, stackAction);
-//
-//    }
-
-    @Override
-    public PushdownAutomaton copy() {
-        PushdownAutomaton copy = new PushdownAutomaton();
-        //copy.stack
-//        copy.stateCounter = this.stateCounter;
-//        copy.terminalStateIndex = this.terminalStateIndex;
-//        copy.states = this.copyStates();
-//        copy.transitions = this.copyTransitions(copy.states);
-//        copy.actions = this.actions; //TODO:May need to be made to copy
-
-
-        return copy;
+    public PushdownAutomaton<T> loop() {
+        return (PushdownAutomaton<T>) super.loop();
     }
 
 }
