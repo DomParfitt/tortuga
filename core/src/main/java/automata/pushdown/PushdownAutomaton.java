a140d478c429776d7ebab9e3335867040751665c
Dom Parfitt <domparfitt89@hotmail.com>
2018-01-15 20:06:13 +0000

    Fixed implementation

--- core/src/main/java/automata/pushdown/PushdownAutomaton.java
+++ core/src/main/java/automata/pushdown/PushdownAutomaton.java
@@ -1,5 +1,8 @@
 package automata.pushdown;
 
+import automata.State;
+import automata.actions.AutomataAction;
+import automata.actions.StackAction;
 import automata.finitestate.FiniteStateMachine;
 import automata.memory.Stack;
 import grammar.Grammar;
@@ -22,28 +25,57 @@ public abstract class PushdownAutomaton<T> extends FiniteStateMachine<T> {
         return stack;
     }
 
+    @Override
+    public boolean parse(List<T> tokens) {
+        return super.parse(tokens) && this.getStack().isEmpty();
+    }
+
+    @Override
+    public boolean parse(T token) {
+        return super.parse(token) && this.getStack().isEmpty();
+    }
+
     public void addStackAction() {
         //TODO: Figure out possible implementation for this
     }
 
     public PushdownAutomaton<T> concatenate(PushdownAutomaton<T> other) {
-        return (PushdownAutomaton<T>) super.concatenate(other);
+        PushdownAutomaton<T> pda = (PushdownAutomaton<T>) super.concatenate(other);
+        pda.updateStackReferences();
+        return pda;
     }
 
     public PushdownAutomaton<T> concatenate(Grammar grammar) {
-        return (PushdownAutomaton<T>)super.concatenate(grammar.getMachine());
+        PushdownAutomaton<T> pda =  (PushdownAutomaton<T>)super.concatenate(grammar.getMachine());
+        pda.updateStackReferences();
+        return pda;
     }
 
     public PushdownAutomaton<T> union(PushdownAutomaton<T> other) {
-        return (PushdownAutomaton<T>) super.union(other);
+        PushdownAutomaton<T> pda = (PushdownAutomaton<T>) super.union(other);
+        pda.updateStackReferences();
+        return pda;
     }
 
     public PushdownAutomaton<T> union(Grammar grammar) {
-        return (PushdownAutomaton<T>) super.union(grammar.getMachine());
+        PushdownAutomaton<T> pda =  (PushdownAutomaton<T>)super.union(grammar.getMachine());
+        pda.updateStackReferences();
+        return pda;
     }
 
     public PushdownAutomaton<T> loop() {
         return (PushdownAutomaton<T>) super.loop();
+    }
+
+    private void updateStackReferences() {
+        for(State state : this.getStates()) {
+            for(AutomataAction<T> action : this.getActions(state)) {
+                if(action instanceof StackAction) {
+                    ((StackAction<T>) action).setStack(this.getStack());
+                }
+            }
+
+        }
     }
 
 }
