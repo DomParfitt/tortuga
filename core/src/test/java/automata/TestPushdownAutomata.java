299d56b4f8df077625246d0c4e67ba012ef13675
Dom Parfitt <dom.parfitt@cgi.com>
2018-01-15 16:21:01 +0000

    Added new tests

--- core/src/test/java/automata/TestPushdownAutomata.java
+++ core/src/test/java/automata/TestPushdownAutomata.java
@@ -1,5 +1,8 @@
 package automata;
 
+import automata.pushdown.ParserMachine;
+import automata.pushdown.Pop;
+import automata.pushdown.Push;
 import automata.pushdown.PushdownAutomaton;
 import grammar.LexerGrammar;
 import grammar.ParserGrammarFactory;
@@ -16,11 +19,41 @@ import static org.junit.Assert.assertTrue;
 public class TestPushdownAutomata {
 
     Lexer lexer;
-    PushdownAutomaton pda;
+    ParserMachine pda;
 
     @Before
     public void setup() {
         lexer = new Lexer();
+    }
+
+    @Test
+    public void testMatchingParens() {
+        pda = new Push(LexerGrammar.OPEN_PAREN);
+        pda = pda.concatenate(new Pop(LexerGrammar.CLOSE_PAREN, LexerGrammar.OPEN_PAREN));
+        System.out.println(pda);
+        List<LexerGrammar> lexemes =new ArrayList<>();
+        lexemes.add(LexerGrammar.OPEN_PAREN);
+        lexemes.add(LexerGrammar.CLOSE_PAREN);
+        assertTrue(pda.parse(lexemes));
+    }
+
+    @Test
+    public void testAnyNumberMatchingParens() {
+        pda = new Push(LexerGrammar.OPEN_PAREN);
+        pda = pda.loop();
+        ParserMachine pop = new Pop(LexerGrammar.CLOSE_PAREN, LexerGrammar.OPEN_PAREN).loop();
+        pda = pda.concatenate(pop);
+        System.out.println(pda);
+        List<LexerGrammar> lexemes =new ArrayList<>();
+        lexemes.add(LexerGrammar.OPEN_PAREN);
+        lexemes.add(LexerGrammar.OPEN_PAREN);
+        lexemes.add(LexerGrammar.OPEN_PAREN);
+
+        lexemes.add(LexerGrammar.CLOSE_PAREN);
+        lexemes.add(LexerGrammar.CLOSE_PAREN);
+        lexemes.add(LexerGrammar.CLOSE_PAREN);
+
+        assertTrue(pda.parse(lexemes));
     }
 
     @Test
